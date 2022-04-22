5327f3f216515f0015ac4a28558301baa4d7f498
Venneti Sri Satya Vinay <81536213+srisatyavinay@users.noreply.github.com>
2022-04-22 16:27:13 +0530

    Final exp - 1

--- experiment/simulation/js/not.js
+++ experiment/simulation/js/not.js
@@ -1,62 +1,56 @@
+'use strict';
+
 function notValid() {
-    checkAndUpdate()
-    modifyOutput()
-    circuitValid()
-    showTruthTable()
-    document.getElementById('error-container').style='display:none;';
-    // document.getElementById('error-container').style.display='flex'
-    // window.setTimeout("document.getElementById('error-container').style='display:none !important;';", 5000)
+    checkAndUpdate();
+    modifyOutput();
+    circuitValid();
+    showTruthTable();
+    document.getElementById('error-container').style = 'display:none;';
 }
 
-function showTruthTable()
-{
+function showTruthTable() {
     const output = [];
-    a = document.getElementById("table-body")
-    k = document.getElementById("input0")
-    if(k.classList.contains("high") && !k.classList.contains("low"))
-    {
-        // console.log("In high")
-        output[1] = getTruthValue()
+    const tableBody = document.getElementById("table-body");
+    const divInput0 = document.getElementById("input0");
+    if (divInput0.classList.contains("high") && !divInput0.classList.contains("low")) {
+        output[1] = getTruthValue();
         listInput[0].input = 0;
-        checkAndUpdate()
-        output[0] = getTruthValue()
-        listInput[0].input = 1;    
-        checkAndUpdate()
-    }
-    if(!k.classList.contains("high") && k.classList.contains("low"))
-    {
-        console.log("In low")
-        output[0] = getTruthValue()
+        checkAndUpdate();
+        output[0] = getTruthValue();
         listInput[0].input = 1;
-        checkAndUpdate()
-        output[1] = getTruthValue()
-        listInput[0].input = 0;
-        checkAndUpdate()
+        checkAndUpdate();
     }
-    a.innerHTML = "<tr><td>0</td><td>1</td><td>" + output[0] + "</td></tr>" + "<tr><td>1</td><td>0</td><td>" + output[1] + "</td></tr>"
+    if (!divInput0.classList.contains("high") && divInput0.classList.contains("low")) {
+        output[0] = getTruthValue();
+        listInput[0].input = 1;
+        checkAndUpdate();
+        output[1] = getTruthValue();
+        listInput[0].input = 0;
+        checkAndUpdate();
+    }
+    tableBody.innerHTML = `<tr><td>0</td><td>1</td><td>${output[0]}</td></tr><tr><td>1</td><td>0</td><td>${output[1]} </td></tr>`;
 }
 
-function modifyOutput()
-{
-    a = document.getElementById("output0")
-    a.innerHTML = 'Output<br>' + getTruthValue()
+function modifyOutput() {
+    const divOutput0 = document.getElementById("output0");
+    divOutput0.innerHTML = 'Output<br>' + getTruthValue();
 }
 
 function permutator(inputArr) {
     const results = [];
 
     function permute(arr, memo) {
-        let cur
+        let currentCase;
 
         memo = memo || [];
 
         for (let i = 0; i < arr.length; i++) {
-            cur = arr.splice(i, 1);
+            currentCase = arr.splice(i, 1);
             if (arr.length === 0) {
-                results.push(memo.concat(cur));
+                results.push(memo.concat(currentCase));
             }
-            permute(arr.slice(), memo.concat(cur));
-            arr.splice(i, 0, cur[0]);
+            permute(arr.slice(), memo.concat(currentCase));
+            arr.splice(i, 0, currentCase[0]);
         }
 
         return results;
@@ -66,34 +60,32 @@ function permutator(inputArr) {
 }
 
 function checkPseudoNmos() {
-    x = permutator([0, 1])
-    console.log("x", x)
-    let psNmosCircuitValid = 0
-    for (let i = 0; i < x.length; i++) {
-        if (connectionMap.has("vdd0$pmos0") && connectionMap.has("ground" + x[i][0] + "$pmos0") && connectionMap.has("pmos0$output0") && connectionMap.has("input0$nmos0") && connectionMap.has("nmos0$output0") && connectionMap.has("ground" + x[i][1] + "$nmos0")) {
-            psNmosCircuitValid = 1
-            break
+    const permutatorMap = permutator([0, 1]);
+    let psNmosCircuitValid = 0;
+    for (let i = 0; i < permutatorMap.length; i++) {
+        if (connectionMap.has("vdd0$pmos0") && connectionMap.has("ground" + permutatorMap[i][0] + "$pmos0") && connectionMap.has("pmos0$output0") && connectionMap.has("input0$nmos0") && connectionMap.has("nmos0$output0") && connectionMap.has("ground" + permutatorMap[i][1] + "$nmos0") && connectionMap.size === 6) {
+            psNmosCircuitValid = 1;
+            break;
         }
     }
-    return psNmosCircuitValid
+    return psNmosCircuitValid;
 }
 
 function circuitValid() {
-    psNmosCircuitValid = checkPseudoNmos()
+    const psNmosCircuitValid = checkPseudoNmos()
     // check if correct nand gate is made using correct components
-    if (selectedTab === currentTab.CMOS && connectionMap.has("vdd0$pmos0") && connectionMap.has("input0$pmos0") && connectionMap.has("pmos0$output0") && connectionMap.has("input0$nmos0") && connectionMap.has("nmos0$output0") && connectionMap.has("ground0$nmos0")) {
-        changeObservation("&#10004; Circuit is correct", 'text-danger', 'text-success')
-    }
-    else if (selectedTab === currentTab.PNMOS && psNmosCircuitValid) {
-        changeObservation("&#10004; Circuit is correct", 'text-danger', 'text-success')
-    }
-    else {
-        changeObservation("&#10060; Circuit is incorrect", 'text-success', 'text-danger')
+    if (selectedTab === currentTab.CMOS && connectionMap.has("vdd0$pmos0") && connectionMap.has("input0$pmos0") && connectionMap.has("pmos0$output0") && connectionMap.has("input0$nmos0") && connectionMap.has("nmos0$output0") && connectionMap.has("ground0$nmos0") && connectionMap.size === 6) {
+        changeObservation("&#10004; Circuit is correct", 'text-danger', 'text-success');
+    } else if (selectedTab === currentTab.PNMOS && psNmosCircuitValid) {
+        changeObservation("&#10004; Circuit is correct", 'text-danger', 'text-success');
+    } else {
+        changeObservation("&#10060; Circuit is incorrect", 'text-success', 'text-danger');
     }
 }
 
 function changeObservation(htmlText, removedClass, addedClass) {
-    document.getElementById("output-box").innerHTML = htmlText
-    document.getElementById("output-box").classList.remove(removedClass)
-    document.getElementById("output-box").classList.add(addedClass)
+    const observationBoxElem = document.getElementById("output-box");
+    observationBoxElem.innerHTML = htmlText;
+    observationBoxElem.classList.remove(removedClass);
+    observationBoxElem.classList.add(addedClass);
 }
