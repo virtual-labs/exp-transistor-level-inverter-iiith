7782aa4e664fc8c88c6f3337258dc3027f22b36c
gautamxyz <gautamghai02@gmail.com>
2022-07-02 18:08:26 +0530

    completed complementary cmos demo

--- experiment/simulation/js/animation-cmos.js
+++ experiment/simulation/js/animation-cmos.js
@@ -4,6 +4,7 @@ import { setCoordinates, fillInputDots, objectDisappear, objectAppear, fillColor
 window.simulationStatus = simulationStatus;
 window.restartCircuit = restartCircuit;
 window.setSpeed = setSpeed;
+window.setInput = setInput;
 // Dimensions of working area
 const circuitBoard = document.getElementById("circuit-board");
 const sidePanels = document.getElementsByClassName("v-datalist-container");
@@ -25,16 +26,11 @@ const SPEED = document.getElementById("speed");
 
 let currPos = 0;
 
-const OBJECTS = [document.getElementById("x"), document.getElementById("y"), document.getElementById("clock"), document.getElementById("a")];
-const ARRAYX = [document.getElementById("x1"), document.getElementById("x2"), document.getElementById("x3"), document.getElementById("x4"), document.getElementById("x5"), document.getElementById("x6"), document.getElementById("x7"), document.getElementById("x8"), document.getElementById("x9"), document.getElementById("x10"), document.getElementById("x11"), document.getElementById("x12")];
-const ARRAYY = [document.getElementById("y1"), document.getElementById("y2"), document.getElementById("y3"), document.getElementById("y4"), document.getElementById("y5"), document.getElementById("y6"), document.getElementById("y7"), document.getElementById("y8"), document.getElementById("y9"), document.getElementById("y10"), document.getElementById("y11"), document.getElementById("y12")];
-const TEXTINPUT = [document.createElementNS(svgns, "text"), document.createElementNS(svgns, "text")];
-const TEXTCLOCK = [document.createElementNS(svgns, "text")];
+const OBJECTS = [document.getElementById("input"), document.getElementById("output")];
+const TEXTINPUT = [document.createElementNS(svgns, "text")];
 const TEXTOUTPUT = [document.createElementNS(svgns, "text")];
-const INPUTDOTS = [document.createElementNS(svgns, "circle"), document.createElementNS(svgns, "circle"), document.createElementNS(svgns, "circle")];
-
-const inputStream = [[0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0], [0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1]];
-const outputStream = [0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0];
+const INPUTDOTS = [document.createElementNS(svgns, "circle"), document.createElementNS(svgns, "circle"), document.createElementNS(svgns, "circle"), document.createElementNS(svgns, "circle")];
+const OUTPUTDOTS = [document.createElementNS(svgns, "circle")];
 
 
 let timeline = gsap.timeline({ repeat: 0, repeatDelay: 0 });
@@ -56,106 +52,38 @@ function instructionBoxInit() {
     });
 }
 
-function setActive(i) {
-    if (i === 0) {
-        ARRAYX[i].style.fill = "#eeeb22";
-        ARRAYY[i].style.fill = "#eeeb22";
-    }
-    else {
-        ARRAYX[i - 1].style.fill = "#29e";
-        ARRAYY[i - 1].style.fill = "#29e";
-        ARRAYX[i].style.fill = "#eeeb22";
-        ARRAYY[i].style.fill = "#eeeb22";
-    }
-}
-
 //initialise input text
 function textIOInit() {
     for (const text of TEXTINPUT) {
         text.textContent = 2;
     }
 }
-//initialise clock text
-function textClockInit() {
-    for (const text of TEXTCLOCK) {
-        text.textContent = 2;
-    }
-}
-
 function outputCoordinates() {
-
-    let xcor = 895;
-    let ycor = 455;
-
-    for (const text of TEXTOUTPUT) {
-        setCoordinates(xcor, ycor, text);
-        svg.append(text);
-    }
+    setCoordinates(646,391,TEXTOUTPUT[0]);
+    svg.append(TEXTOUTPUT[0]);
 }
 
-
-function inputDots() {
-    //sets the coordinates of the input dots
-    for (const inputDot of INPUTDOTS) {
-        fillInputDots(inputDot, 200, 200, 15, "#FF0000");
-        svg.append(inputDot);
-    }
-}
-function dotsDisappear() {
+function inputDotsDisappear() {
     for (const inputDot of INPUTDOTS) {
         objectDisappear(inputDot);
     }
 }
-function dotsAppear() {
+function inputDotsAppear() {
     for (const inputDot of INPUTDOTS) {
         objectAppear(inputDot);
     }
 }
-
-function computeXorOne() {
-    if (inputStream[0][currPos] === inputStream[1][currPos]) {
-        fillColor(INPUTDOTS[1], "#eeeb22");
-    }
-    else {
-        fillColor(INPUTDOTS[1], "#29e");
-    }
+function outputDotsDisappear(){
+    for (const outputDot of OUTPUTDOTS) {
+        objectDisappear(outputDot);
+    }   
+}
+function outputDotsAppear(){
+    for (const outputDot of OUTPUTDOTS) {
+        objectAppear(outputDot);
+    }   
 }
 
-function getXor(a,b)
-{
-    if(a === b)
-    {
-        return 0;
-    }
-    return 1;
-}
-
-function computeXorTwo() {
-    if (currPos === 0) {
-        fillColor(INPUTDOTS[2], "#eeeb22");
-    }
-    else {
-        if (getXor(inputStream[0][currPos],inputStream[1][currPos]) === outputStream[currPos - 1]) {
-            fillColor(INPUTDOTS[2], "#eeeb22");
-        }
-        else {
-            fillColor(INPUTDOTS[2], "#29e");
-        }
-    }
-}
-
-
-function xDotDisappear() {
-    objectDisappear(INPUTDOTS[0]);
-}
-
-function yDotDisappear() {
-    objectDisappear(INPUTDOTS[1]);
-}
-
-function ffDotDisapper() {
-    objectDisappear(INPUTDOTS[2]);
-}
 
 // function to disappear the output text
 function outputDisappear() {
@@ -164,121 +92,67 @@ function outputDisappear() {
     }
 }
 // function to appear the input text
-function outputVisible() {
-    for (const text of TEXTOUTPUT) {
-        objectAppear(text);
-    }
-}
-function xTextDisappear() {
-    objectDisappear(TEXTINPUT[0]);
-}
-function xTextAppear() {
-    objectAppear(TEXTINPUT[0]);
-}
-function yTextDisappear() {
-    objectDisappear(TEXTINPUT[1]);
-}
-function yTextAppear() {
-    objectAppear(TEXTINPUT[1]);
-}
-function clockDisappear() {
-    objectDisappear(TEXTCLOCK[0]);
-}
-function clockAppear() {
-    objectAppear(TEXTCLOCK[0]);
-}
 function clearObservation() {
     OBSERV.innerHTML = EMPTY;
 }
 function allDisappear() {
-    dotsDisappear();
-    xTextDisappear();
-    yTextDisappear();
-    clockDisappear();
+    inputDotsDisappear();
+    outputDotsDisappear();
+    objectDisappear(TEXTINPUT[0]);
     outputDisappear();
     for (const object of OBJECTS) {
         fillColor(object, "#008000");
     }
 }
-function outputHandler() {
-    let state = currPos;
-    TEXTOUTPUT[0].textContent = outputStream[state];
-    setter(TEXTOUTPUT[0].textContent, OBJECTS[3]);
-    setter(outputStream[state], INPUTDOTS[2]);
-}
-function changeInput() {
-    let state = currPos;
-    TEXTINPUT[0].textContent = inputStream[0][state];
-    TEXTINPUT[1].textContent = inputStream[1][state];
-    setCoordinates(45, 440, TEXTINPUT[0]);
-    setCoordinates(45, 490, TEXTINPUT[1]);
-    svg.append(TEXTINPUT[0]);
-    svg.append(TEXTINPUT[1]);
-    setActive(state);
-    setter(TEXTINPUT[0].textContent, OBJECTS[0]);
-    setter(TEXTINPUT[1].textContent, OBJECTS[1]);
-    if (inputStream[0][state] === 1) {
-        fillColor(INPUTDOTS[0], "#29e");
-    }
-    else {
-        fillColor(INPUTDOTS[0], "#eeeb22");
-    }
-    if (inputStream[1][state] === 1) {
-        fillColor(INPUTDOTS[1], "#29e");
-    }
-    else {
-        fillColor(INPUTDOTS[1], "#eeeb22");
-    }
-    if (state === 0) {
-        fillColor(INPUTDOTS[2], "#eeeb22");
-    }
+
+function setInputDots(){
+    setter("1",INPUTDOTS[1]);
+    setter("0",INPUTDOTS[3]);
 }
 
-function clockToZero() {
-    TEXTCLOCK[0].textContent = 0;
-    svg.appendChild(TEXTCLOCK[0]);
-    setCoordinates(598, 704, TEXTCLOCK[0]);
-    fillColor(OBJECTS[2], "#eeeb22");
-    OBSERV.innerHTML = "Clock set to 0";
-}
-function clockToOne() {
-    TEXTCLOCK[0].textContent = 1;
-    svg.appendChild(TEXTCLOCK[0]);
-    setCoordinates(598, 704, TEXTCLOCK[0]);
-    fillColor(OBJECTS[2], "#29e");
-    OBSERV.innerHTML = "Clock set to 1";
+function setInput() {
+    if (TEXTINPUT[0].textContent !== "0" && timeline.progress() === 0) {
+        changeTo0(145, 390, 0, 0);
+    }
+    else if (TEXTINPUT[0].textContent !== "1" && timeline.progress() === 0) {
+        changeTo1(145, 390, 0, 0);
+    }
+    setter(TEXTINPUT[0].textContent,INPUTDOTS[0]);
+    setter(TEXTINPUT[0].textContent,INPUTDOTS[2]);
 }
 
-function increaseCurrPos() {
-    currPos++;
+function changeTo1(coordinateX, coordinateY, object, textObject) {
+    TEXTINPUT[textObject].textContent = 1;
+    svg.appendChild(TEXTINPUT[textObject]);
+    setCoordinates(coordinateX, coordinateY, TEXTINPUT[textObject]);
+
+    fillColor(OBJECTS[object], "#29e");
+    objectAppear(TEXTINPUT[0])
+    clearObservation();
+}
+
+function changeTo0(coordinateX, coordinateY, object, textObject) {
+    TEXTINPUT[textObject].textContent = 0;
+    svg.appendChild(TEXTINPUT[textObject]);
+    setCoordinates(coordinateX, coordinateY, TEXTINPUT[textObject]);
+
+    fillColor(OBJECTS[object], "#eeeb22");
+    objectAppear(TEXTINPUT[0])
+    clearObservation();
 }
 
 function reboot() {
-    for (const elements of ARRAYX) {
-        elements.style.fill = "#29e";
-    }
-    for (const elements of ARRAYY) {
-        elements.style.fill = "#29e";
-    }
     for (const text of TEXTINPUT) {
-        text.textContent = 2;
-    }
-    for (const text of TEXTCLOCK) {
         text.textContent = 2;
     }
 }
 function display() {
     OBSERV.innerHTML = "Simulation has finished. Press Reset to start again";
-    OBSERV.innerHTML += "<br />";
-    let img = document.createElement("img");
-    img.src = "./images/state-table.png";
-    OBSERV.appendChild(img);
 }
 function setter(value, component) {
     //toggles the text content a of input/output component b
     if (value === "1") {
         unsetColor(component);
-
     }
     else if (value === "0") {
         setColor(component);
@@ -287,8 +161,7 @@ function setter(value, component) {
 
 
 function changeSpeed(newSpeed) {
-
-    if (TEXTINPUT[0].textContent !== "2" && TEXTINPUT[1].textContent !== "2" && timeline.progress() !== 1) {
+    if (TEXTINPUT[0].textContent !== "2" && timeline.progress() !== 1) {
         timeline.resume();
         timeline.timeScale(newSpeed);
         OBSERV.innerHTML = newSpeed + "x speed";
@@ -298,10 +171,8 @@ function changeSpeed(newSpeed) {
 }
 function setSpeed(speed) {
     if (circuitStarted) {
-
-
         if (speed === "1") {
-            startCircuit();
+            changeSpeed(1);
         }
         else if (speed === "2") {
             changeSpeed(2);
@@ -310,33 +181,30 @@ function setSpeed(speed) {
             changeSpeed(4);
         }
     }
-
-
 }
 function restartCircuit() {
     if (!circuitStarted) {
         circuitStarted = true;
     }
+
     timeline.seek(0);
     timeline.pause();
     allDisappear();
     reboot();
-    currPos = 0;
     clearObservation();
     decide = false;
     STATUS.innerHTML = "Start";
     OBSERV.innerHTML = "Successfully restored";
     SPEED.selectedIndex = 0;
+
 }
 
 function simulationStatus() {
     if (!decide) {
         startCircuit();
-
     }
     else if (decide) {
         stopCircuit();
-
     }
 }
 function stopCircuit() {
@@ -351,94 +219,87 @@ function stopCircuit() {
         OBSERV.innerHTML = "Please Restart the simulation";
     }
 }
-// need to fix this up a bit
 function startCircuit() {
     if (!circuitStarted) {
         circuitStarted = true;
     }
-    timeline.play();
-    timeline.timeScale(1);
-    OBSERV.innerHTML = "Simulation has started.";
-    decide = true;
-    STATUS.innerHTML = "Pause";
-    SPEED.selectedIndex = 0;
+    if (TEXTINPUT[0].textContent !== "2") {
+        timeline.play();
+        timeline.timeScale(1);
+        OBSERV.innerHTML = "Simulation has started.";
+        decide = true;
+        STATUS.innerHTML = "Pause";
+        SPEED.selectedIndex = 0;
+    }
+    else {
+        OBSERV.innerHTML = "Please set the value of input to 0 or 1";
+    }
     if (timeline.progress() === 1) {
         OBSERV.innerHTML = "Please Restart the simulation";
     }
 }
 
-function simulator() {
+function inputDots() {
+    //sets the coordinates of the input dots
+    for (const inputDot of INPUTDOTS) {
+        fillInputDots(inputDot, 200, 200, 15, "#FF0000");
+        svg.append(inputDot);
+    }
+}
+function outputDots() {
+    //sets the coordinates of the input dots
+    for (const outputDot of OUTPUTDOTS) {
+        fillInputDots(outputDot, 200, 200, 15, "#FF0000");
+        svg.append(outputDot);
+    }
+}
+
+function simulator2(){
+    if(TEXTINPUT[0].textContent === "0"){
+        setter("1",OUTPUTDOTS[0]);
+        timeline.to(OUTPUTDOTS[0], {
+            motionPath: {
+                path: "#path3",
+                align: "#path3",
+                autoRotate: true,
+                alignOrigin: [0.5, 0.5]
+            },
+    
+            duration: 4,
+            delay: 5,
+            repeat: 0,
+            repeatDelay: 3,
+            yoyo: true,
+            ease: "none",
+            paused: false,
+    
+        }, 0);
+    }
+    else{
+        setter("0",OUTPUTDOTS[0]);
+        timeline.to(OUTPUTDOTS[0], {
+            motionPath: {
+                path: "#path4",
+                align: "#path4",
+                autoRotate: true,
+                alignOrigin: [0.5, 0.5]
+            },
+    
+            duration: 4,
+            delay: 5,
+            repeat: 0,
+            repeatDelay: 3,
+            yoyo: true,
+            ease: "none",
+            paused: false,
+    
+        }, 0);
+    }
+}
+
+
+function simulator1() {
     timeline.to(INPUTDOTS[0], {
-        motionPath: {
-            path: "#path3",
-            align: "#path3",
-            autoRotate: true,
-            alignOrigin: [0.5, 0.5]
-        },
-
-        duration: 4,
-        delay: 12 * currPos,
-        repeat: 0,
-        repeatDelay: 3,
-        yoyo: true,
-        ease: "none",
-        paused: false,
-
-    }, 0);
-    timeline.to(INPUTDOTS[1], {
-        motionPath: {
-            path: "#path4",
-            align: "#path4",
-            autoRotate: true,
-            alignOrigin: [0.5, 0.5]
-        },
-
-        duration: 4,
-        delay: 12 * currPos,
-        repeat: 0,
-        repeatDelay: 3,
-        yoyo: true,
-        ease: "none",
-        paused: false,
-
-    }, 0);
-    timeline.to(INPUTDOTS[2], {
-        motionPath: {
-            path: "#path7",
-            align: "#path7",
-            autoRotate: true,
-            alignOrigin: [0.5, 0.5]
-        },
-
-        duration: 8,
-        delay: 12 * currPos,
-        repeat: 0,
-        repeatDelay: 3,
-        yoyo: true,
-        ease: "none",
-        paused: false,
-
-    }, 0);
-
-    timeline.to(INPUTDOTS[1], {
-        motionPath: {
-            path: "#path5",
-            align: "#path5",
-            autoRotate: true,
-            alignOrigin: [0.5, 0.5]
-        },
-
-        duration: 4,
-        delay: 12 * currPos + 4,
-        repeat: 0,
-        repeatDelay: 3,
-        yoyo: true,
-        ease: "none",
-        paused: false,
-
-    }, 0);
-
-    timeline.to(INPUTDOTS[2], {
         motionPath: {
             path: "#path6",
             align: "#path6",
@@ -447,7 +308,58 @@ function simulator() {
         },
 
         duration: 4,
-        delay: 12 * currPos + 8,
+        delay: 0,
+        repeat: 0,
+        repeatDelay: 3,
+        yoyo: true,
+        ease: "none",
+        paused: false,
+
+    }, 0);
+    timeline.to(INPUTDOTS[1], {
+        motionPath: {
+            path: "#path1",
+            align: "#path1",
+            autoRotate: true,
+            alignOrigin: [0.5, 0.5]
+        },
+
+        duration: 4,
+        delay: 0,
+        repeat: 0,
+        repeatDelay: 3,
+        yoyo: true,
+        ease: "none",
+        paused: false,
+
+    }, 0);
+    timeline.to(INPUTDOTS[2], {
+        motionPath: {
+            path: "#path5",
+            align: "#path5",
+            autoRotate: true,
+            alignOrigin: [0.5, 0.5]
+        },
+
+        duration: 4,
+        delay: 0,
+        repeat: 0,
+        repeatDelay: 3,
+        yoyo: true,
+        ease: "none",
+        paused: false,
+
+    }, 0);
+    timeline.to(INPUTDOTS[3], {
+        motionPath: {
+            path: "#path2",
+            align: "#path2",
+            autoRotate: true,
+            alignOrigin: [0.5, 0.5]
+        },
+
+        duration: 4,
+        delay: 0,
         repeat: 0,
         repeatDelay: 3,
         yoyo: true,
@@ -457,42 +369,42 @@ function simulator() {
     }, 0);
 }
 
+function outputHandler(){
+    if(TEXTINPUT[0].textContent === "0"){
+        TEXTOUTPUT[0].textContent = "1";
+    }
+    else{
+        TEXTOUTPUT[0].textContent = "0";
+    }
+    objectAppear(TEXTOUTPUT[0]);
+    setter(TEXTOUTPUT[0].textContent,OBJECTS[1]);
+}
+
+
 //execution starts here
 gsap.registerPlugin(MotionPathPlugin);
 demoWidth();
 instructionBoxInit();
 textIOInit();
-textClockInit();
 outputCoordinates();
+inputDotsDisappear();
+outputDotsDisappear();
 inputDots();
+outputDots();
 outputDisappear();
-// calling all the functions that are going to initialise 
-timeline.add(clockToZero, 0);
-timeline.add(clockAppear, 0);
-timeline.add(xTextAppear, 0);
-timeline.add(yTextAppear, 0);
 
-for (let i = 0; i < 144; i += 12) {
-    timeline.add(changeInput, 0 + i);
-    if (i % 24 === 0) {
-        timeline.add(clockToZero, 0 + i);
-    }
-    else {
-        timeline.add(clockToOne, 0 + i);
-    }
-    timeline.add(dotsAppear, 0 + i);
-    timeline.add(simulator, 0 + i);
-    timeline.add(xDotDisappear, 4 + i);
-    timeline.add(computeXorOne, 4 + i);
-    timeline.add(yDotDisappear, 8 + i);
-    timeline.add(computeXorTwo, 8 + i);
-    timeline.add(dotsDisappear, 12 + i);
-    timeline.add(outputHandler, 12 + i);
-    timeline.add(outputVisible, 12 + i);
-    timeline.add(increaseCurrPos, 12 + i);
-}
-timeline.add(display, 146);
-timeline.eventCallback("onComplete", outputVisible);
+// calling all the functions that are going to initialise
+timeline.add(setInputDots,0);
+timeline.add(inputDotsAppear,0);
+timeline.add(simulator1,0);
+timeline.add(inputDotsDisappear,5);
+timeline.add(outputDotsAppear,5);
+timeline.add(simulator2,5);
+timeline.add(outputDotsDisappear,9);
+timeline.add(outputHandler,9);
+timeline.add()
+timeline.add(display, 10);
 timeline.eventCallback("onComplete", display);
 timeline.pause();
-dotsDisappear();
+inputDotsDisappear();
+outputDotsDisappear();
